e2e8e67134d3a557b817835e47027532fd832490
Sam Bolgert <sbolgert@gmail.com>
2014-08-04 22:34:51 -0700

    first commit

---
+++ hellodjango/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+#from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'hellodjango.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'earth.views.home', name='home')
+)
